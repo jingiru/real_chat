d6f10d16b3c4be0686ac3f3344cd0d026d5c92a0
Jingiru <skyjjw79@hanmail.net>
2020-06-09 15:54:35 +0900

    Start Studying about chatting App

---
+++ app/src/main/java/com/example/real_chat/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.real_chat;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button = (Button)findViewById(R.id.btn_login);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
